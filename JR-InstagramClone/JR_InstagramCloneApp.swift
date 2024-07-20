f7e4134f382e5cba81794804e9dc515ab6e64435
Fatih <fatihozen247@gmail.com>
2024-07-20 14:32:16 +0300

    Initial Commit

---
+++ JR-InstagramClone/JR_InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  JR_InstagramCloneApp.swift
+//  JR-InstagramClone
+//
+//  Created by Fatih Özen on 20.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct JR_InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
